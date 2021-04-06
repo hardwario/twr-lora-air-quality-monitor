b76b52883d7e58002024853757188161e7402207
Jakub Smejkal <smejkal.jakub99@gmail.com>
2021-04-06 18:32:32 +0200

    PlatformIO and TWR prefix update

--- include/application.h
+++ include/application.h
@@ -5,48 +5,49 @@
 #define VERSION "vdev"
 #endif
 
+#include <twr.h>
 #include <bcl.h>
 
 typedef struct
 {
     uint8_t channel;
     float value;
-    bc_tick_t next_pub;
+    twr_tick_t next_pub;
 
 } event_param_t;
 
 typedef struct
 {
-    bc_tag_temperature_t self;
+    twr_tag_temperature_t self;
     event_param_t param;
 
 } temperature_tag_t;
 
 
-typedef struct 
+typedef struct
 {
-    bc_tag_voc_lp_t tag_voc_lp;
+    twr_tag_voc_lp_t tag_voc_lp;
     event_param_t param;
 } voc_lp_tag_t;
 
 
 typedef struct
 {
-    bc_tag_humidity_t self;
+    twr_tag_humidity_t self;
     event_param_t param;
 
 } humidity_tag_t;
 
 typedef struct
 {
-    bc_tag_lux_meter_t self;
+    twr_tag_lux_meter_t self;
     event_param_t param;
 
 } lux_meter_tag_t;
 
 typedef struct
 {
-    bc_tag_barometer_t self;
+    twr_tag_barometer_t self;
     event_param_t param;
 
 } barometer_tag_t;
